878c42d7280a36669b1740d2b7207541716cb1e6
Dener Alves Andrade <dener.andrade@codate.com.br>
2015-07-01 20:44:48 -0300

    principal listagem com imagem que nao deu certo

--- xita-web/src/Xita/ValidarLogin.java
+++ xita-web/src/Xita/ValidarLogin.java
@@ -28,6 +28,7 @@ public class ValidarLogin implements Serializable {
 	private ClienteDAO validarCliente;
 	@Inject
 	private Conversation conversation;
+	private String usuario, senha;
 	public FacesContext getContext() {
 		return context;
 	}
@@ -53,7 +54,8 @@ public class ValidarLogin implements Serializable {
 	  }
 	
 	public String validar() {
-		
+		c.setEmail(getUsuario());
+		c.setSenha(getSenha());
 		idClienteSessao = validarCliente.validarCliente(c);
 
 		if (idClienteSessao != -1) {
@@ -119,6 +121,22 @@ public class ValidarLogin implements Serializable {
 
 	public void setSession(HttpSession session) {
 		this.session = session;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 
 
