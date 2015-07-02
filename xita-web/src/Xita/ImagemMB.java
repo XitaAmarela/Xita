5ff1cb8faa6affb4f472da519eb7c73668eccd0f
Dener Alves Andrade <dener.andrade@codate.com.br>
2015-07-01 21:12:56 -0300

    principal

--- xita-web/src/Xita/ImagemMB.java
+++ xita-web/src/Xita/ImagemMB.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.PhaseId;
 
@@ -24,14 +23,16 @@ public class ImagemMB {
 	public StreamedContent getImage() throws IOException {
 		FacesContext context = FacesContext.getCurrentInstance();
 
-		
+		if (context.getCurrentPhaseId() == PhaseId.RENDER_RESPONSE) {
+			return new DefaultStreamedContent();
+		} else {
 			String fotoId = context.getExternalContext().getRequestParameterMap().get("fotoId");
 			if (fotoId.equals("")) {
 				return null;
 			}
 			ImagemProduto f = cadastroFoto.buscarPorId(new Long(fotoId));
 			return new DefaultStreamedContent(new ByteArrayInputStream(f.getContent()), f.getTipo());
-		
+		}
 	}
 
 
